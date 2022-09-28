951cb2a9eb7914eeb8910b77e22c5ec2ac4ba797
trosner <tannerrosner@gmail.com>
2022-09-27 19:43:33 -0700

    css update, .gitignore, html buttons, etc.

--- WindagoWebApplication/WindagoWebApplication/wwwroot/js/site.js
+++ WindagoWebApplication/WindagoWebApplication/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function clickMe() {
+    var rows = document.getElementsByClassName("row");
+    for (let i = 0; i <= rows.length - 1; i++) {
+        var row = rows[i];
+        row.textContent = "HELLO YOU!";
+        row.style.color = "blue";
+    }
+}
