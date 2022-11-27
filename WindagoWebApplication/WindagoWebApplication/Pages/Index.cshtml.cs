fdd581a417cc04fda904e5097f554b349611e022
trosner <tannerrosner@gmail.com>
2022-11-26 18:38:21 -0800

    model and dataset functions

--- WindagoWebApplication/WindagoWebApplication/Pages/Index.cshtml.cs
+++ WindagoWebApplication/WindagoWebApplication/Pages/Index.cshtml.cs
@@ -1,20 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
 
-namespace WindagoWebApplication.Pages
-{
-    public class IndexModel : PageModel
-    {
+namespace WindagoWebApplication.Pages {
+    public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
+        public IndexModel(ILogger<IndexModel> logger) {
             _logger = logger;
         }
 
-        public void OnGet()
-        {
-
+        public void OnGetBasePage() {
+            StringBuilder sb = new StringBuilder();
         }
     }
 }
