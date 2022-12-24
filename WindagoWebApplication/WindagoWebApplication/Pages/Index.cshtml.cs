94ccded8b67a8e981a599aa4139c205524792783
trosner <tannerrosner@gmail.com>
2022-12-23 20:50:48 -0800

    save point. spaghetti code testing

--- WindagoWebApplication/WindagoWebApplication/Pages/Index.cshtml.cs
+++ WindagoWebApplication/WindagoWebApplication/Pages/Index.cshtml.cs
@@ -6,6 +6,8 @@ namespace WindagoWebApplication.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
 
+        private Boolean isChecked = false;
+
         public IndexModel(ILogger<IndexModel> logger) {
             _logger = logger;
         }
